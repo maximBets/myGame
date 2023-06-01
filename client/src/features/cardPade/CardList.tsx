26d68e5d6345a11fe13eb28dc20553cd66aedb25
Maxim <maxbeat040@gmail.com>
2023-06-01 19:06:56 +0300

    подготовлен вывод карты

--- client/src/features/cardPade/CardList.tsx
+++ client/src/features/cardPade/CardList.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Card from './Card';
+import { RootState } from '../../store';
 
-function CardList():JSX.Element {
+function CardList(): JSX.Element {
+  const s = useSelector((store: RootState) => store);
 
   return (
     <div>
       {/* {themes.map(theme => (<div className="">тема</div>)
       flashes.map(flash => <Card />))} */}
-      
     </div>
   );
 }
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,10 +1,12 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { combineReducers, createStore } from 'redux';
 import flashCardReducer from './features/cardPade/reducer/reducer';
 
 const store = createStore(
   combineReducers({
     flashCardReducer,
-  })
+  }),
+  composeWithDevTools()
 );
 
 export default store;
