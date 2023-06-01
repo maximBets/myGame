6fd4fc95da63d61cc0cbdba641e7ff8c35705a04
Alexxx2173 <Alexxx.Aksenov@gmail.com>
2023-06-01 19:04:33 +0300

    get_themes_router_create

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const serverConfig = require('./config/serverConfig');
 
+const cardPageApiRouter = require('./api/api.routes');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -11,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 serverConfig(app);
 
 //* Routing
-
+app.use('/api', cardPageApiRouter);
 //* Listening
 app.listen(PORT, () => {
   console.log(`Server started by ${PORT} port`);
