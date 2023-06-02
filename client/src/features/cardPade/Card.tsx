6b157018e786fb2bb81e264337395220908f6210
Maxim <maxbeat040@gmail.com>
2023-06-02 10:39:57 +0300

    скилет модалки

--- client/src/features/cardPade/Card.tsx
+++ client/src/features/cardPade/Card.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './css/styles.module.css';
+import { Flash } from './reducer/types/type';
+import './Modal.css';
 
-function Card(): JSX.Element {
+function Card({ flash }: { flash: Flash }): JSX.Element {
+  const [active, setActive] = useState(false);
+
+  console.log(active);
+
   return (
-    <div className={styles.containerCard}>
-      <div>500</div>
-    </div>
+    <>
+      <div className={styles.containerCard} onClick={() => setActive(!active)}>
+        <div className={styles.scoreCard}>{flash.score} </div>
+      </div>
+      <div>
+        <div className={active ? 'modals active' : 'modals'}>
+          <div className={active ? 'modals-content active' : 'modals'}>
+            <div className="">{flash.question}</div>
+            <input></input>
+            <button type="button" onClick={() => setActive(!active)}>
+              Закрыть
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
